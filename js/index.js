efedf2983088f785c505d52e3d181d894029efae
Madushan Perera <madushanperera3817@gmail.com>
2020-08-14 00:26:15 +0530

    new Styles

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 import "../scss/style.scss";
+
+document.getElementById("scrollable").onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.getElementById("scrollable").scrollTop;
+  var height =
+    document.getElementById("scrollable").scrollHeight -
+    document.getElementById("scrollable").clientHeight;
+  var scrolled = (winScroll / height) * 100;
+
+  document.getElementById("pBar").style.height = scrolled + "%";
+}
